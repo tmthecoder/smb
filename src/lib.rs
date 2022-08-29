0fcb85c05660cd7dca6f30e6fc9b5eabbbeb86a2
Tejas Mehta <tmthecoder@gmail.com>
2022-08-29 12:21:17 -0400

    move over command code parsing to the header file

--- src/lib.rs
+++ src/lib.rs
@@ -14,82 +14,6 @@ pub struct SMBConnection {
     stream: TcpStream
 }
 
-#[repr(u8)]
-#[derive(Debug, Eq, PartialEq, TryFromPrimitive)]
-pub enum SMBMessageCommandCode {
-    CreateDirectory,
-    DeleteDirectory,
-    Open,
-    Create,
-    Close,
-    Flush,
-    Delete,
-    Rename,
-    QueryInformation,
-    SetInformation,
-    Read,
-    Write,
-    LockByteRange,
-    UnlockByteRange,
-    CreateTemporary,
-    CreateNew,
-    CheckDirectory,
-    ProcessExit,
-    Seek,
-    LockAndRead,
-    WriteAndUnlock,
-    ReadRaw = 0x1A,
-    ReadMPX,
-    ReadMPXSecondary,
-    WriteRaw,
-    WriteMPX,
-    WriteMPXSecondary,
-    WriteComplete,
-    QueryServer,
-    SetInformation2,
-    QueryInformation2,
-    LockingANDX,
-    Transaction,
-    TransactionSecondary,
-    IOCTL,
-    IOCTLSecondary,
-    Copy,
-    Move,
-    Echo,
-    WriteAndClose,
-    OpenANDX,
-    ReadANDX,
-    WriteANDX,
-    NewFileSize,
-    CloseAndTreeDisc,
-    Transaction2,
-    Transaction2Secondary,
-    FindClose2,
-    FindNotifyClose,
-    TreeConnect = 0x70,
-    TreeDisconnect,
-    Negotiate,
-    SessionSetupANDX,
-    LogoffANDX,
-    TreeConnectANDX,
-    QueryInformationDisk = 0x80,
-    Search,
-    Find,
-    FindUnique,
-    FindClose,
-    NTTransact = 0xA0,
-    NTTransactSecondary,
-    NTCreateANDX,
-    NTCancel,
-    NTRename,
-    OpenPrintFile = 0xC0,
-    WritePrintFile,
-    ClosePrintFile,
-    GetPrintQueue,
-    ReadBulk = 0xD9,
-    WriteBulkData
-}
-
 pub struct SMBConnectionIterator<'a> {
     server: &'a SMBServer
 }
@@ -117,19 +41,6 @@ impl SMBConnection {
     }
 }
 
-impl SMBMessageCommandCode {
-    fn parse(data: &[u8]) -> Option<Self> {
-        println!("data: {:?} len: {}", data, data.len());
-        if let Some(pos) = data.iter().position(|x| *x == 'S' as u8) {
-            if data[pos..].starts_with(b"SMB") {
-                println!("GOT SMB: {}", pos);
-                return SMBMessageCommandCode::try_from(data[pos + 3]).ok()
-            }
-        }
-        None
-    }
-}
-
 impl Iterator for SMBConnectionIterator<'_> {
     type Item = SMBConnection;
 
@@ -143,18 +54,18 @@ impl Iterator for SMBConnectionIterator<'_> {
     }
 }
 
-impl Iterator for SMBMessageIterator<'_> {
-    type Item = SMBMessageCommandCode;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut buffer = [0_u8; 128];
-        let mut carryover = [0_u8; 128];
-        match self.stream.stream.read(&mut buffer) {
-            Ok(read) => SMBMessageCommandCode::parse(&buffer[0..read]),
-            _ => None
-        }
-    }
-}
+// impl Iterator for SMBMessageIterator<'_> {
+//     type Item = SMBMessageCommandCode;
+//
+//     fn next(&mut self) -> Option<Self::Item> {
+//         let mut buffer = [0_u8; 128];
+//         let mut carryover = [0_u8; 128];
+//         match self.stream.stream.read(&mut buffer) {
+//             Ok(read) => SMBMessageCommandCode::parse(&buffer[0..read]),
+//             _ => None
+//         }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
--- src/parser/message_header.rs
+++ src/parser/message_header.rs
@@ -10,6 +10,82 @@ struct SMBHeader {
     mid: u16
 }
 
+#[repr(u8)]
+#[derive(Debug, Eq, PartialEq, TryFromPrimitive)]
+pub enum SMBCommandCode {
+    CreateDirectory,
+    DeleteDirectory,
+    Open,
+    Create,
+    Close,
+    Flush,
+    Delete,
+    Rename,
+    QueryInformation,
+    SetInformation,
+    Read,
+    Write,
+    LockByteRange,
+    UnlockByteRange,
+    CreateTemporary,
+    CreateNew,
+    CheckDirectory,
+    ProcessExit,
+    Seek,
+    LockAndRead,
+    WriteAndUnlock,
+    ReadRaw = 0x1A,
+    ReadMPX,
+    ReadMPXSecondary,
+    WriteRaw,
+    WriteMPX,
+    WriteMPXSecondary,
+    WriteComplete,
+    QueryServer,
+    SetInformation2,
+    QueryInformation2,
+    LockingANDX,
+    Transaction,
+    TransactionSecondary,
+    IOCTL,
+    IOCTLSecondary,
+    Copy,
+    Move,
+    Echo,
+    WriteAndClose,
+    OpenANDX,
+    ReadANDX,
+    WriteANDX,
+    NewFileSize,
+    CloseAndTreeDisc,
+    Transaction2,
+    Transaction2Secondary,
+    FindClose2,
+    FindNotifyClose,
+    TreeConnect = 0x70,
+    TreeDisconnect,
+    Negotiate,
+    SessionSetupANDX,
+    LogoffANDX,
+    TreeConnectANDX,
+    QueryInformationDisk = 0x80,
+    Search,
+    Find,
+    FindUnique,
+    FindClose,
+    NTTransact = 0xA0,
+    NTTransactSecondary,
+    NTCreateANDX,
+    NTCancel,
+    NTRename,
+    OpenPrintFile = 0xC0,
+    WritePrintFile,
+    ClosePrintFile,
+    GetPrintQueue,
+    ReadBulk = 0xD9,
+    WriteBulkData
+}
+
 enum SMBStatus {
     NTStatus(NTStatusLevel),
     DosError(char, char, u16)
@@ -37,4 +113,17 @@ enum SMBFlags2 {
 
 enum SMBExtra {
 
+}
+
+impl SMBCommandCode {
+    fn parse(data: &[u8]) -> Option<Self> {
+        println!("data: {:?} len: {}", data, data.len());
+        if let Some(pos) = data.iter().position(|x| *x == 'S' as u8) {
+            if data[pos..].starts_with(b"SMB") {
+                println!("GOT SMB: {}", pos);
+                return SMBCommandCode::try_from(data[pos + 3]).ok()
+            }
+        }
+        None
+    }
 }
