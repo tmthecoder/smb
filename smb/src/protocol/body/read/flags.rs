b129e34594e061489c95007837ad99d0e4736f23
Tejas Mehta <tmthecoder@gmail.com>
2024-04-08 13:20:13 -0400

    Add read response

--- smb/src/protocol/body/read/flags.rs
+++ smb/src/protocol/body/read/flags.rs
@@ -1,16 +1,26 @@
 use bitflags::bitflags;
+use num_enum::TryFromPrimitive;
 use serde::{Deserialize, Serialize};
+
+use smb_derive::{SMBByteSize, SMBFromBytes, SMBToBytes};
 
 use crate::util::flags_helper::{impl_smb_byte_size_for_bitflag, impl_smb_from_bytes_for_bitflag, impl_smb_to_bytes_for_bitflag};
 
 bitflags! {
     #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
-    pub struct SMBReadFlags: u8 {
+    pub struct SMBReadRequestFlags: u8 {
         const UNBUFFERED = 0x01;
         const REQUEST_COMPRESSED = 0x02;
     }
 }
 
-impl_smb_from_bytes_for_bitflag!(SMBReadFlags);
-impl_smb_to_bytes_for_bitflag!(SMBReadFlags);
-impl_smb_byte_size_for_bitflag!(SMBReadFlags);
+#[repr(u32)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize, SMBToBytes, SMBFromBytes, SMBByteSize, TryFromPrimitive)]
+pub enum SMBReadResponseFlags {
+    None = 0x0,
+    RdmaTransform = 0x01,
+}
+
+impl_smb_from_bytes_for_bitflag!(SMBReadRequestFlags);
+impl_smb_to_bytes_for_bitflag!(SMBReadRequestFlags);
+impl_smb_byte_size_for_bitflag!(SMBReadRequestFlags);
--- smb/src/protocol/body/read/mod.rs
+++ smb/src/protocol/body/read/mod.rs
@@ -1,10 +1,12 @@
+use std::marker::PhantomData;
+
 use serde::{Deserialize, Serialize};
 
 use smb_derive::{SMBByteSize, SMBFromBytes, SMBToBytes};
 
 use crate::protocol::body::create::file_id::SMBFileId;
 use crate::protocol::body::read::channel::SMBRWChannel;
-use crate::protocol::body::read::flags::SMBReadFlags;
+use crate::protocol::body::read::flags::{SMBReadRequestFlags, SMBReadResponseFlags};
 
 mod flags;
 pub mod channel;
@@ -13,7 +15,7 @@ pub mod channel;
 #[smb_byte_tag(49)]
 pub struct SMBReadRequest {
     #[smb_direct(start(fixed = 3))]
-    flags: SMBReadFlags,
+    flags: SMBReadRequestFlags,
     #[smb_direct(start(fixed = 4))]
     length: u32,
     #[smb_direct(start(fixed = 8))]
@@ -28,4 +30,17 @@ pub struct SMBReadRequest {
     remaining_bytes: u32,
     #[smb_buffer(offset(inner(start = 44, num_type = "u16", subtract = 64)), length(inner(start = 46, num_type = "u16")))]
     channel_information: Vec<u8>,
+}
+
+#[derive(Debug, PartialEq, Eq, SMBByteSize, SMBToBytes, SMBFromBytes, Serialize, Deserialize)]
+#[smb_byte_tag(17)]
+pub struct SMBReadResponse {
+    #[smb_skip(start = 3, length = 1)]
+    reserved: PhantomData<Vec<u8>>,
+    #[smb_direct(start(fixed = 8))]
+    data_remaining: u32,
+    #[smb_direct(start(fixed = 12))]
+    flags: SMBReadResponseFlags,
+    #[smb_buffer(order = 0, offset(inner(start = 2, num_type = "u8", subtract = 64)), length(inner(start = 4, num_type = "u32")))]
+    data: Vec<u8>,
 }
